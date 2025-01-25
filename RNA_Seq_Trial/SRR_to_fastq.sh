0485ca62e3f00e0b78e82adce96dc3810105ad83
NawaneetanS <s.nawaneetan2002@gmail.com>
2025-01-25 23:34:48 +0530

    new changes

--- RNA_Seq_Trial/SRR_to_fastq.sh
+++ RNA_Seq_Trial/SRR_to_fastq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Input file for Acc list
-Acc_List="/media/nannu1375/Briefcase/Actrec/Bioinfo/RNA_Seq_Trial/Multiple_AccNo.txt"
+Acc_List="/media/nannu1375/Briefcase/Actrec/Bioinfo/RNA_Seq_Trial/SRR_Acc_List.txt"
 
 ## Output dir for fastq files
 OutDir="/media/nannu1375/Briefcase/Actrec/Bioinfo/RNA_Seq_Trial/SRR_fastq"
@@ -9,12 +9,13 @@ mkdir -p "$OutDir"
 
 ## Read Acc list line by line
 while IFS= read -r ACC; do
-	echo "Downloading FASTQ for SRR$ACC..."
-	fasterq-dump "SRR$ACC" -O "$OutDir" --split-files
+	echo "Downloading FASTQ for $ACC..."
+	prefetch $ACC
+	fasterq-dump "$ACC" -O "$OutDir" --split-files
 	if [ $? -ne 0 ]; then
-	  echo "Error downloading SRR$ACC. Skipping..."
+	  echo "Error downloading $ACC. Skipping..."
 	else
-	  echo " Successfully downloaded SRR$ACC."
+	  echo " Successfully downloaded $ACC."
 	fi
 done < "$Acc_List"
 
